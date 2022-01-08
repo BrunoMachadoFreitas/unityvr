515f271147993f09de255cf02a869fef43788602
BrunoMMachadoFreitas <senhorragnaros@gmail.com>
2022-01-08 10:48:55 +0000

    hack
    hackkk

---
+++ Druid/Assets/cod_movimentoBola.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cod_movimentoBola : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeanTween.moveX(gameObject, 9, 3).setEase(LeanTweenType.easeInOutSine).setDelay(2f); ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Druid/Assets/cod_teleporta.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cod_teleporta : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void movimentaTeleport()
+    {
+        
+        LeanTween.move(player, new Vector3 (transform.position.x, transform.position.y + 3.5f, transform.position.z) , 3).setEase(LeanTweenType.easeInOutSine);
+
+    }
+}
