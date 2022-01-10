8ffceedc1a3e602c1f6c2e8f961c8a2e8cf0d360
BrunoMMachadoFreitas <senhorragnaros@gmail.com>
2022-01-10 21:45:13 +0000

    progress bar
    pb

---
+++ Druid/Assets/Scripts/mostraInfoPlanta.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mostraInfoPlanta : MonoBehaviour
+{
+    public GameObject progressBar;
+    public float timeLeft;
+    public float tempoInicio = 0;
+    public bool podeContar = false;
+    public Image imageProgressBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tempoInicio = timeLeft;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (podeContar && timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime * 3 ;
+            imageProgressBar.fillAmount = timeLeft / tempoInicio;
+        }
+    }
+
+    public void mostraProgressBar()
+    {
+        progressBar.gameObject.SetActive(true);
+        podeContar = true;
+       
+    }
+   
+    public void exitPlanta()
+    {
+        progressBar.gameObject.SetActive(false);
+        podeContar = false;
+    }
+
+    IEnumerator contaProgress()
+    {
+        //wait 5 seconds
+        yield return new WaitForSeconds(100);
+
+        //destroy this gameobject
+        Destroy(this.gameObject);
+    }
+}
