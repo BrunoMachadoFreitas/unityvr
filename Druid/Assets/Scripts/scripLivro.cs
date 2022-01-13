220a57de3456c78fa893182defdb1f007464eb3e
BrunoMMachadoFreitas <senhorragnaros@gmail.com>
2022-01-13 15:50:03 +0000

    livro

---
+++ Druid/Assets/Scripts/scripLivro.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scripLivro : MonoBehaviour
+{
+    public GameObject Livro;
+
+    public void fazAnimacao()
+    {
+        Livro.GetComponent<Animator>().Play("animaCubo");
+    }
+}
