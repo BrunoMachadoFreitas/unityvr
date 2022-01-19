fe22973635f732c308b69617eeeec232882edcf7
FrangoGhost <franciscochicharo@live.com.pt>
2022-01-19 17:38:15 +0000

    Base Intro

---
+++ Druid/Assets/Scripts/codigo_troca_de_scene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class codigo_troca_de_scene : MonoBehaviour
+{
+    public GameObject player;
+    public string scene;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void movimentaTeleport_e_Scene()
+    {
+
+        LeanTween.move(player, new Vector3(transform.position.x, transform.position.y + 3.5f, transform.position.z), 3).setEase(LeanTweenType.easeInOutSine).setOnComplete(()=>Application.LoadLevel(scene));
+        
+
+    }
+}
