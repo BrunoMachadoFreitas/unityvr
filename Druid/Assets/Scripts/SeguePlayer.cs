10ef14db842f93888023758b54d6a58a0e6ac9a5
BrunoMMachadoFreitas <senhorragnaros@gmail.com>
2022-01-21 17:49:16 +0000

    livro

---
+++ Druid/Assets/Scripts/SeguePlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public Vector3 offset = new Vector3(-1.8f, 0.04f, 3.91f);
+
+
+    private void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
