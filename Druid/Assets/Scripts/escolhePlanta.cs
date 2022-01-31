3029ebff90c0cdf78ef24d3fba0a38ba1acae838
BrunoMMachadoFreitas <senhorragnaros@gmail.com>
2022-01-31 20:05:47 +0000

    plantar

--- Druid/Assets/Scripts/escolhePlanta.cs
+++ Druid/Assets/Scripts/escolhePlanta.cs
@@ -6,12 +6,15 @@ public class escolhePlanta : MonoBehaviour
 {
    
     public GameObject primeiroAguentador;
-    public GameObject aguentador;
+    public GameObject placa;
     public GameObject vasos;
+    public PlantsName plantaEscolhida;
+    private BoxCollider boxPlaca;
+    public Canvas canvasPrimeiro;
     // Start is called before the first frame update
     void Start()
     {
-        
+        boxPlaca = placa.GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
@@ -22,10 +25,13 @@ public class escolhePlanta : MonoBehaviour
 
     public void metePlanta()
     {
-        Instantiate(aguentador, primeiroAguentador.transform.position, Quaternion.identity);
+        boxPlaca.enabled = false;
+        canvasPrimeiro.enabled = false;
+        placa.GetComponent<cod_plant_contoler>().plantName = plantaEscolhida;
+        Instantiate(placa, primeiroAguentador.transform.position, Quaternion.identity);
+
        
-        aguentador.GetComponentInChildren<cod_plant_contoler>().plantName = PlantsName.Noveleiro;
-        primeiroAguentador.gameObject.SetActive(false);
+       // primeiroAguentador.gameObject.SetActive(false);
 
 
         vasos.gameObject.SetActive(false);
